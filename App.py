493501c645ecf452538612d26031ea312a3978ad
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2022-03-26 13:52:28 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,14 @@
+from numpy import delete
+import pandas as pd
+import csv
+
+df = pd.read_csv('planet.csv')
+print(df.shape)
+
+del df["Luminosity"]
+del df["Drawf Star Name"]
+del df["Dist"]
+del df["Weight"]
+del df["Rad"]
+
+df.to_csv('stars.csv')
